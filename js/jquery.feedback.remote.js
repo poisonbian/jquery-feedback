76ea0cfb21f29fba82d36dd4646fef4f89ab1e68
poisonbian <poisonbian@gmail.com>
2014-01-27 15:07:58 +0800

    去除暂时无用的主题css；修改feedback js文件名称
    Change-Id: I7d80474ce1fd788a431dc97ca042c61d9109f887
    Signed-off-by: poisonbian <poisonbian@gmail.com>

---
+++ js/jquery.feedback.remote.js
@@ -0,0 +1,33 @@
+function loadScript(url, judge, callback) {
+	if (judge != null) {
+		judge = eval(judge);
+		if (judge) {
+			callback();
+			return;
+		}
+	}
+	
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.readyState ? script.onreadystatechange = function() {
+        if (script.readyState == "loaded" || script.readyState == "complete") {
+        	script.onreadystatechange = null;
+        	callback();
+        }
+    } : script.onload = function() {
+        callback();
+    };
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+loadScript("http://libs.baidu.com/jquery/1.8.3/jquery.min.js", "window.jQuery", function() {
+	loadScript("http://libs.baidu.com/json/json2/json2.js", "window.JSON", function () {
+		loadScript("js/jquery.feedback.js", null, function() {
+	    	loadScript("test/jquery.feedback.test.js", null, function() {
+
+	        });
+	    });
+	});    
+});
+
